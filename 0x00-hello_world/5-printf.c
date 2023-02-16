acbbd478f610e490814b53fbcbad73a6a708796c
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-16 03:11:11 -0800

    added successfull commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,12 @@
 
 /*
  * main - Entry point
- * description: this is added
+ *
+ * Description: 'ths is added'
+ * 
  * return - success 0
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
