70c28b39e1b6b235325a5ba1c1c9dd262bb28255
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-16 02:46:10 -0800

    added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * Main -Entry point
+ * Main -Entry
  * return - leaves the main functions aka end of programe
 */
-
 int main(void)
 {  
 	puts("\"Programming is like building a multilingual puzzle");
