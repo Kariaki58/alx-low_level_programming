738cf7021898a26d0ba82f6752ec6dc1a3f3a01c
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-17 05:57:34 -0800

    code to print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
