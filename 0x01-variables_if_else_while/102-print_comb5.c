b37a84475712d20b6cc3ff626ff5cb52f7ed379d
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-17 20:36:25 -0800

    hope i am done with all the problems

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i1, i2, j1, j2;
+
+	for (i1 = 0; i1 < 10; i1++)
+	{
+		for (i2 = 0; i2 < 9; i2++)
+		{
+			for (j1 = 0; j1 < 10; j1++)
+			{
+				for (j2 = 0; j2 < 10; j2++)
+				{
+				putchar(i1 + '0');
+				putchar(i2 + '0');
+				putchar(' ');
+				putchar(j1 + '0');
+				putchar(j2 + '0');
+				if (j1 == 9 && j2 == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
