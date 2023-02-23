2f336e88834683bba10174c5366fa1a270124f9a
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-23 10:36:50 -0800

    function to print a number from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * more_numbers - returns a number from 1 to 14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			_putchar((j / 10) + '0');
+		}
+		_putchar((j % 10) + '0');
+	}
+	_putchar('\n');
+}
