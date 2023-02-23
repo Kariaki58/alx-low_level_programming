681a0db5e0b6231a7d85daf271a3554e0e0feff7
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-23 13:17:00 -0800

    fizzbuss practices

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i < 2; i++)
+	{
+		for (j = 1; j <= 100; j++)
+		{
+			if ((j % 3 == 0) && (j % 5 == 0))
+			{
+				printf("FizzBuzz ");
+				continue;
+			}
+			if (j % 3 == 0)
+			{
+				printf("Fizz ");
+				continue;
+			}
+			if (j % 5 == 0)
+			{
+				printf("Buzz ");
+				continue;
+			}
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+	return (0);
+}
