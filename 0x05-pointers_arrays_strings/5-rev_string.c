8bb5de6aa98951be20fc79819fbaa33c87a8912a
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-27 02:20:15 -0800

    added a function to reverse a string inplace

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - the function that reverse the string s
+ * @s: the input to be revered.
+ */
+void rev_string(char arr[])
+{
+	int i, j;
+
+	i = 0;
+	j = strlen(arr) - 1;
+	while (i < j)
+	{
+		char tem;
+
+		tem = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tem;
+		i++;
+		j--;
+	}
+}
