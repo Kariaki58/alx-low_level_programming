2f2a230209f9a2bdc7a31299b2a77230b1f38f7d
Kariaki58 <kariakichernocherno@gmail.com>
2023-02-27 11:47:11 -0800

    function to learn using strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - function to copy a string from src to dest
+ * @dest: where the string goes
+ * @src: where the string comes from.
+ * Return: string_dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
