5d788e2e95b6ab62bc9a8e72bbbe182ab6da750a
Kariaki58 <kariakichernocherno@gmail.com>
2023-03-01 13:10:33 -0800

    added a function to capitalize all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <string.h>
 #include "main.h"
 /**
  * cap_string - capitalizes all words of a string.
@@ -6,28 +8,21 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int len = strlen(str);
+	int capitalize_next = 1;
+	int i;
 
-	while (str[i])
+	for (i = 0; i < len; i++)
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z))
-			i++;
-		if (str[i - 1] == ' ' ||
-		str[i - 1] == '\t' || 
-		str[i - 1] == '\n' || 
-		str[i - 1] == ',' || 
-		str[i - 1] == ';' ||
-		str[i - 1] == '.' ||
-		str[i - 1] == '!' ||
-		str[i - 1] == '?' ||
-		str[i - 1] == '"' ||
-		str[i - 1] == '(' ||
-		str[i - 1] == ')' ||
-		str[i - 1] == '{' ||
-		str[i - 1] == '{' ||
-		i == 0)
-			str[i] -= 32;
-		i++;
+		if (isspace(str[i]) || ispunct(str[i]))
+		{
+			capitalize_next = 1;
+		}
+		else if (capitalize_next)
+		{
+			str[i] = toupper(str[i]);
+			capitalize_next = 0;
+		}
 	}
 	return (str);
 }
