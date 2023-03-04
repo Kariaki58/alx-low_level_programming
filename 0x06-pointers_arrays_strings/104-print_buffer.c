b965145dd46129dd5effb02ebfaa742b1aaf5c2e
Kariaki58 <kariakichernocherno@gmail.com>
2023-03-04 10:03:26 -0800

    print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,52 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - function to print out buffer
- * @b: function input
- * @size: function input 2
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int byte, index;
 
-	for (i = 0; i < size; i += 10)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("%08x: ", i);
-		for (j = i; j < i + 10 && j < size; j++)
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
 		{
-			printf("%02x", (unsigned char)b[j]);
-			if (j % 2 == 1)
-			{
+			if ((index + byte) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
 				printf(" ");
-			}
 		}
-		for (; j < i + 10; j++)
+
+		for (index = 0; index < 10; index++)
 		{
-			printf(" ");
-			if (j % 2 == 1)
-			{
-				printf(" ");
-			}
-		}
-		printf(" ");
-		for (j = i; j < i + 10 && j < size; j++)
-		{
-			if (b[j] >= ' ' && b[j] <= '~')
-			{
-				printf("%c", b[j]);
-			}
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
 			else
 				printf(".");
 		}
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
 	}
-	printf("\n");
+
+	if (size <= 0)
+		printf("\n");
 }
