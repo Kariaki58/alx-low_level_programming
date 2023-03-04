ab62538f98086b3c555a16ba40eb77beb3025a45
Kariaki58 <kariakichernocherno@gmail.com>
2023-03-03 19:30:21 -0800

    betty is not working

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,26 +1,32 @@
 #include "main.h"
-
 /**
- * infinite_add - Adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: Buffer to store the result
- * @size_r: Buffer size
+ * infinite_add - Adds two numbers.
  *
- * Return: Pointer to the result, or 0 if result can't be stored in r
+ * @n1: The first number.
+ * @n2: The second number.
+ * @r: The buffer where the result will be stored.
+ * @size_r: The size of the buffer.
+ *
+ * Return: If the result can be stored in r - a pointer to the result.
+ *         If the result can't be stored in r - 0.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k = 0, l1 = 0, l2 = 0, sum = 0, carry = 0;
-	char *tmp;
+	int i, j, k, carry, sum, len1, len2;
 
-	while (n1[l1 + 1])
-		l1++;
-	while (n2[l2 + 1])
-		l2++;
-	if (l1 > size_r || l2 > size_r)
+	len1 = 0;
+	while (n1[len1])
+		len1++;
+	len2 = 0;
+	while (n2[len2])
+		len2++;
+	if (len1 + 2 > size_r || len2 + 2 > size_r)
 		return (0);
-	for (i = l1, j = l2; i >= 0 || j >= 0; i--, j--, k++)
+	carry = 0;
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
+	while (i >= 0 || j >= 0)
 	{
 		sum = carry;
 		if (i >= 0)
@@ -29,23 +35,24 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum += n2[j] - '0';
 		carry = sum / 10;
 		r[k] = sum % 10 + '0';
+		j--;
+		i--;
+		k++;
 	}
 	if (carry)
 	{
+		if (k + 1 > size_r)
+			return (0);
 		r[k] = carry + '0';
-		r[k + 1] = '\0';
-		tmp = r;
+		k++;
 	}
-	else
-	{
-		r[k] = '\0';
-		tmp = r + k - 1;
-	}
+	r[k] = '\0';
 	for (i = 0, j = k - 1; i < j; i++, j--)
 	{
-		tmp[i] ^= tmp[j];
-		tmp[j] ^= tmp[i];
-		tmp[i] ^= tmp[j];
+		char tmp = r[i];
+
+		r[i] = r[j];
+		r[j] = tmp;
 	}
 	return (r);
 }
