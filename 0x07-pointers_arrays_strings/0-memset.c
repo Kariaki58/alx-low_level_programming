7fe8b57fdd384287d76580b4862190399839b245
Kariaki58 <kariakichernocherno@gmail.com>
2023-03-07 01:04:55 -0800

    hello memeset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,10 +5,13 @@
  * @s: pointer to char params
  * @b: data to change
  * @n: index
+ *
  * Return: *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (memset(s, b, n));
+	char *ptr = memset(s, b, n);
+
+	return ptr;
 }
