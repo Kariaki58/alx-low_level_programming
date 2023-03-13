736a0d0f8193964b875d4742a4b91a77eedce3b8
Kariaki58 <kariakichernocherno@gmail.com>
2023-03-13 01:42:28 -0700

    task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,10 @@ char *create_array(unsigned int size, char c)
 	if (size)
 	{
 		char *mem_alloc;
-		int i = 0;
+		unsigned int i;
 
-		for (; i < size; i++)
+		mem_alloc = malloc(size);
+		for (i = 0; i < size; i++)
 		{
 			mem_alloc[i] = c;
 		}
