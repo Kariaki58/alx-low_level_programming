601e9862a10d31673f3dd4f33b685cc94c685bc2
Kariaki58 <kariakichernocherno@gmail.com>
2023-03-20 07:50:36 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef __MAIN__
 #define __MAIN__
-
-struct dog {
+/**
+ * struct dog - Short description
+ * @name: string data
+ * @age: age data
+ * @owner: owner data
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
