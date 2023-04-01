fef3ba8af42cfb72856695de5998adf9298e16dc
Kariaki58 <kariakichernocherno@gmail.com>
2023-04-01 11:26:34 -0700

    task x

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,11 +12,7 @@ int main(void)
 	fib_2 = 2;
 	printf("%ld, ", fib_1);
 	printf("%ld, ", fib_2);
-<<<<<<< HEAD
-	for (i = 0; i <= 98; i++)
-=======
 	for (i = 0; i < 98 - 2; i++)
->>>>>>> 783871730da5675738a1a2ec21eea2a34f487113
 	{
 		fib_cal = fib_1 + fib_2;
 		fib_1 = fib_2;
