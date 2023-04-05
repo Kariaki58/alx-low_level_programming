9a5b790792cf34ef6e2fa4eda597122b43f7a7da
Kariaki58 <kariakichernocherno@gmail.com>
2023-04-05 15:19:43 -0700

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - return the address of the new element or NULL
+ * @head: the start of the node
+ * @str: string input of the node
+ * Return: return
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (NULL);
+	}
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
+	return (new_node);
+}
