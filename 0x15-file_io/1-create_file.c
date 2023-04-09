583a802d70c29dbf7e1a1d116249f778e487064b
Kariaki58 <kariakichernocherno@gmail.com>
2023-04-08 21:52:27 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		len = strlen(text_content);
 		write_file = write(_file, text_content, len);
-		if (write_file != len)
+		if (len != write_file)
 			return (-1);
 	}
 	close(_file);
