a5b9298deac45fc66a020f70dc744569b3d04b72
Kariaki58 <kariakichernocherno@gmail.com>
2023-04-11 08:45:46 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	unsigned int i;
+	unsigned int num, i;
+	size_t str_len;
+	int char_num;
 
-	if (b == NULL)
+	num = 0;
+	str_len = strlen(b);
+	if (!b)
 		return (0);
-	for (i = 0; i < strlen(b); i++)
+	for (i = 0; i < str_len; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		num = (num << 1) + (b[i] - '0');
+		char_num = b[i] - '0';
+		num += char_num << (str_len - 1 - i);
 	}
 	return (num);
 }
