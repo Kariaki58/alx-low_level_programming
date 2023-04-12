490814a8c258a83e40997ce7e6122fe625324d41
Kariaki58 <kariakichernocherno@gmail.com>
2023-04-11 20:05:32 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ * clear_bit - clear bit
+ * @n: address of a number
+ * @index: index of the number
+ * Return: 1 or -1
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n))
 		return (-1);
 	*n = *n & (~(1 << index));
+	return (1);
 }
