118bcf914ec68af4eb3c36c9a668b5beabc39e0c
Kariaki58 <kariakichernocherno@gmail.com>
2023-05-01 20:54:09 -0700

    prints all number os base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
