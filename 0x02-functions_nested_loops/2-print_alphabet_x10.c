7cbb4538727552ee4f46932017fff7b70dd97b29
Kariaki58 <kariakichernocherno@gmail.com>
2023-05-07 16:24:43 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_alphabet - get_alphabet
+ */
+void get_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		get_alphabet();
+}
