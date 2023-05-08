499b66b4ca666b9d8b1279ee09fcf4008ff7e26d
Kariaki58 <kariakichernocherno@gmail.com>
2023-05-07 18:44:57 -0700

    9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,26 +12,25 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			mul = i * j;
-			if (mul > 9)
-			{
-				_putchar((mul / 10) + '0');
-				_putchar((mul % 10) + '0');
-				if (j < 9)
-				{
-				_putchar(',');
-				_putchar(' ');
-				}
-			}
-			if (mul < 10)
+			mul = j * i;
+			if (j == 0)
 			{
 				_putchar(mul + '0');
-				if (j < 9)
-				{
+			}
+
+			if (mul < 10 && j != 0)
+			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				}
+				_putchar(mul + '0');
+			}
+			else if (mul >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
 			}
 		}
 		_putchar('\n');
