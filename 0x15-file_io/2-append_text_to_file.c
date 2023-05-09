15864d435db270e35930e01aabcadd74b7c3dfd2
Kariaki58 <kariakichernocherno@gmail.com>
2023-05-09 13:31:05 -0700

    commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,16 +14,19 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int write_file;
+	int write_file, len = 0;
 
 	if (!filename)
 		return (-1);
+	if (text_content)
+	{
+		while(text_content[len])
+			len++;
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
-	if (!text_content)
-		return (-1);
-	write_file = write(fd, text_content, strlen(text_content) - 2);
+	write_file = write(fd, text_content, len);
 	if (write_file < 0)
 		return (-1);
 	close(fd);
