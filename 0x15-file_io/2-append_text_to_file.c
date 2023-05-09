e86f9602113b0f56b5a9e269baf147bf660604e9
Kariaki58 <kariakichernocherno@gmail.com>
2023-05-09 13:06:53 -0700

    function to ppend text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,6 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
+	if (!text_content)
+		return (-1);
 	write_file = write(fd, text_content, strlen(text_content));
 	if (write_file < 0)
 		return (-1);
