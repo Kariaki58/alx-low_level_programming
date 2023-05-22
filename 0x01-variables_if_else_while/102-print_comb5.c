1dfe98b7a1f038fd074581837a45a7944e12888c
Kariaki58 <programmerjavascript901@gmail.com>
2023-05-22 19:39:11 +0100

    program to print a number from 0 - 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,21 +13,20 @@ int main(void)
 	{
 		for (j = i; j < 100; j++)
 		{
-			if ((i % 10) == (j % 10))
-				continue;
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 98 || j != 99)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
-		if (status)
-			break;
 	}
 	putchar('\n');
 	return (0);
