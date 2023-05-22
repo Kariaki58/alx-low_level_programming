31f08de6579d19c2ba23f414dc66259bac06d9ec
Kariaki58 <programmerjavascript901@gmail.com>
2023-05-22 19:09:13 +0100

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,5 +7,31 @@
  */
 int main(void)
 {
+	int i, j, status = 0;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			
+			if ((i % 10) == (j % 10))
+				continue;
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98)
+			{
+				status = 1;
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		if (status)
+			break;
+	}
+	putchar('\n');
 	return (0);
 }
