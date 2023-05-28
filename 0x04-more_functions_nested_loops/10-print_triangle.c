bd3b7c98cfae3975a70f12c7618ae77de4ebf26a
Kariaki58 <programmerjavascript901@gmail.com>
2023-05-28 15:27:04 +0100

    function that prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - 1; j > i; j--)
+			_putchar(' ');
+		for (k = 0; k <= i; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,4 +13,5 @@ void num_ones(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 #endif
