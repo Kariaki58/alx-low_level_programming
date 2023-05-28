c14c4c6147d18d0c101b6a0dda9afac6b095fac7
Kariaki58 <programmerjavascript901@gmail.com>
2023-05-28 13:52:45 +0100

    function that prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,29 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers
+ * num_ones - print the number from 0 - 14
+ */
+void num_ones(void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		if (i > 9)
+			_putchar((i / 10) + '0');
+		_putchar(( i % 10) + '0');
+	}
+}
+/**
+ * more_numbers - prints 14 times the numbers
  */
 void more_numbers(void)
 {
 	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		num_ones();
+		_putchar('\n');
+	}
 }
