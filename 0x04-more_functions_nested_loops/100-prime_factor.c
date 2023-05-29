17fd508b39cbe21dd0bf2d6384edd3975238f149
Kariaki58 <programmerjavascript901@gmail.com>
2023-05-29 05:27:59 +0100

    program that finds and prints the largest prime factor of a number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i;
+	long unsigned int prime_number = 612852475143;
+	long unsigned int max_prime = -1;
+
+	while (prime_number % 2 == 0)
+	{
+		max_prime = 2;
+		prime_number /= 2;
+	}
+	for (i = 3; i <= sqrt(prime_number); i += 2)
+	{
+		while (prime_number % i == 0)
+		{
+			max_prime = i;
+			prime_number /= i;
+		}
+	}
+	if (prime_number > 2)
+		max_prime = prime_number;
+	printf("%ld\n", max_prime);
+	return (0);
+}
