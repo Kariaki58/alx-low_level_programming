4e234dbfad03103427035c243faab44afbd1e7d5
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-15 07:54:01 -0700

    function that frees a double linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the double linked list
+ * @head: head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head, *prev = head;
+
+	if (head == NULL)
+		return;
+	while(temp != NULL)
+	{
+		prev = temp;
+		temp = temp->next;
+		free(prev);
+	}
+}
