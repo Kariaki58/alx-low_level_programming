0645b4b910544ee72f2c508a287d9fa7610b4f53
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-15 07:34:52 -0700

    added null to the next of the linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode = malloc(sizeof(dlistint_t));
 
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
 	newnode->prev = NULL;
 	newnode->next = NULL;
