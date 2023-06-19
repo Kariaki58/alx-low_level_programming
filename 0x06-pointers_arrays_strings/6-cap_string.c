555d2dcdb7c22093ffcac290f2c4b444c52e7afe
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-19 10:57:38 -0700

    program that capitalize a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * cap_string - capitalize any string you see in str
+ * @str: string to be capitalize
+ * Return: return capitalized str
+ */
+char *cap_string(char str[])
+{
+	char *d = ",;.!?(){}";
+	unsigned int i = 1, j, data;
+
+	while (str[i])
+	{
+		for (j = 0; j < strlen(d) - 1; j++)
+		{
+			if (str[i - 1] == d[j] || str[i - 1] == ' ' || \
+			str[i - 1] == '\t' || str[i - 1] == '"' || \
+			str[i - 1] == '\n')
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					data = str[i];
+					data -= 32;
+					str[i] = data ;
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}
