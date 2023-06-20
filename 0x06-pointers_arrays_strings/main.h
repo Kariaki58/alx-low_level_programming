74fd6c2edd749871effefdc6959cc6da44acd3cb
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-20 06:05:19 +0200

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,9 +9,6 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-<<<<<<< HEAD
 char *rot13(char *str);
-=======
 char *cap_string(char *);
->>>>>>> aabe1809bf0e0dac1c3ff41ab4f3b33759e5ad85
 #endif /* Header */
