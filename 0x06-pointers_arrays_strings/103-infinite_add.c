b494af3569695fe630e71266ada16690d4383240
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-22 15:28:08 +0100

    just trying

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,31 +12,34 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int max_len = (len1 > len2) ? len1 : len2;
-	int carry = 0;
 	int sum = 0;
-	int i, j;
+	int max_len = 0;
+	int i, j, carry = 0;
+	int len_1 = strlen(n1);
+	int len_2 = strlen(n2);
 
-	if (max_len + 1 > size_r)
+	if (len_1 > len_2)
+		max_len = len_1;
+	else
+		max_len = len_2;
+	if (max_len + 1 >= size_r)
 		return (0);
 	r[max_len + 1] = '\0';
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+	for (i = len_1 - 1, j = len_2 - 1; i >= 0 || j >= 0; i--, j--)
 	{
 		sum = carry;
 		if (i >= 0)
 			sum += n1[i] - '0';
 		if (j >= 0)
 			sum += n2[j] - '0';
-		r[max_len] = ((sum % 10) + '0');
+		r[max_len] = (sum % 10) + '0';
 		carry = sum / 10;
 		max_len--;
 	}
 	if (carry)
 	{
-		if (max_len == -1)
-			return 0;
+		if (max_len < 0)
+			return (0);
 		r[max_len] = carry + '0';
 	}
 	else if (max_len == 0)
