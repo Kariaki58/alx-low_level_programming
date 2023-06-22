282fccc9ea4670216423d5b1bd49f96b61fa1671
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-22 14:01:10 +0100

    add function

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int max_len = (len1 > len2) ? len1 : len2;
+	int carry = 0;
+	int sum = 0;
+	int i, j;
+
+	if (max_len + 1 > size_r)
+		return 0;
+	r[max_len + 1] = '\0';
+	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+		r[max_len] = ((sum % 10) + '0');
+		carry = sum / 10;
+		max_len--;
+	}
+	if (carry)
+	{
+		if (max_len == -1)
+			return 0;
+		r[max_len] = carry + '0';
+	}
+	else if (max_len == 0)
+		return r + 1;
+	return r;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ char *string_toupper(char *);
 char *rot13(char *str);
 char *cap_string(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif /* Header */
