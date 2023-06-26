9ff877ea093f83f14db97f1a8da1de1cc47603a0
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-26 05:13:47 +0100

    solved all task

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	for (j = 0; j < size; j += 10)
+	{
+		printf("%08x: ", j);
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + j) >= size)
+				putchar(' ');
+			else
+				printf("%02x", *(b + i + j));
+			if ((i % 2) != 0 && i != 0)
+				putchar(' ');
+		}
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + j) >= size)
+				break;
+			else if (*(b + i + j) >= 31 && *(b + i + j) <= 126)
+				printf("%c", *(b + i + j));
+			else
+				putchar('.');
+		}
+		if (i >= size)
+			continue;
+		putchar('\n');
+	}
+	if (size <= 0)
+		putchar('\n');
+}
