4f4e87f09bf499416680fa70e5d3c63349e6ccad
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-10 12:44:11 +0100

    function that get the data from a hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the value at an index
+ * @ht: hash table
+ * @key: hash key
+ * Return: data
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (!ht)
+		return (NULL);
+	index = key_index((const unsigned char *) key, ht->size);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
