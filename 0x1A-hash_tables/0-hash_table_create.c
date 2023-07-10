f3ce75ef7f1a152758d3165313f63a9fc51ce6d8
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-10 09:31:38 +0100

    code the function with betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,25 +3,23 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: hash table size
- * Return: return hash table address.
- */
+ * hash_table_create - create a hash table
+ * @size: size of the table.
+ * Return: returns a hash table.
+*/
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_t = malloc(sizeof(hash_node_t));
-    unsigned long int i = 0;
+	hash_table_t *hash_t;
+	unsigned long int i = 0;
 
-    if (!hash_t)
-        return (NULL);
-    hash_t->size = size;
-    hash_t->array = malloc(size * sizeof(hash_node_t *));
-    if (!(hash_t->array))
-    {
-        free(hash_t);
-        return (NULL);
-    }
-    while (i < size)
-        hash_t->array[i++] = NULL;
-    return (hash_t);
+	hash_t = malloc(sizeof(hash_node_t));
+	if (!hash_t)
+		return (NULL);
+	hash_t->size = size;
+	hash_t->array = malloc(size * sizeof(hash_node_t *));
+
+	if (!(hash_t->array))
+		return (NULL);
+	return (hash_t);
 }
