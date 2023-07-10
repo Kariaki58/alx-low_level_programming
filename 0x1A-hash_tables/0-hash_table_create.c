2c3894afd5549913106cc37b678196eec5516d42
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-09 21:41:47 -0700

    added readme file and created the header file and the hashtable fille

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: hash table size
+ * Return: return hash table address.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+Learning HashTable from ALX
