342e8d7a54b3b8af7cff0a5093cb2e238d7c5279
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-12 12:10:50 +0100

    refactor code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,12 +6,11 @@
  * hash_table_create - creates a new hash table
  * @size: size of the table
  * Return: pointer to the hash table
-*/
-
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-			
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
