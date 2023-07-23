0e43303cb1daee1758eb90b055e28520d4c9f535
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-23 15:04:52 +0100

    fix my code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,11 +12,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
 			putchar(a[i][j]);
-		}
-		if (*a[0] == ' ')
-			putchar('\n');
 		putchar('\n');
 	}
 }
