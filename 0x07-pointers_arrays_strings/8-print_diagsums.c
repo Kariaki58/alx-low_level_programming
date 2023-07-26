e7610c5e7e5a6f20f05943850cc2d364824e402f
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-26 09:01:59 +0100

    removed a loop

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,11 +8,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int rows, cols, sum_right = 0, sum_left = 0;
+	int rows, sum_right = 0, sum_left = 0;
 
 	for (rows = 0; rows < size; rows++)
+	{
 		sum_right += a[(rows * size) + rows];
-	for (cols = 0; cols < size; cols++)
-	sum_left += a[(cols * (size - 1)) + (size - 1)];
+		sum_left += a[((size - 1) * rows) + (size - 1)];
+	}
 	printf("%d, %d\n", sum_right, sum_left);
 }
