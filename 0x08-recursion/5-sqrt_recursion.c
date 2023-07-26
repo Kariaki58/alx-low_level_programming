932699a534195ae3ad223fbed352dd68ae1addaf
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-26 13:12:28 +0100

    fixed betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * check_square - check if x can give me a square
+ * @n: number
+ * @x: x
+ * @y: y
+ * Return: return check for square
+ */
 int check_square(int n, int x, int y)
 {
 	if ((x * x) == n)
 		return (x);
 	if (x >= n / 2 || x < 0)
 		return (-1);
-	return check_square(n, x + 1, y);
+	return (check_square(n, x + 1, y));
 }
+
 /**
  * _sqrt_recursion - square root of a number
  * @n: number
