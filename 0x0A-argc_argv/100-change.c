c77385d95399894a9fc2d8e1ad9ef49207f8eb26
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-31 12:46:35 +0100

    okay

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - main point
+ * @ac: ac
+ * @av: av
+ * Return: return 0.
+ */
+int main(int ac, char **av)
+{
+
+	return (0);
+}
