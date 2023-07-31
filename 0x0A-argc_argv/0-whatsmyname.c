363f7fa26aa2c827addcfb0acf2bc627f5350711
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-31 11:33:40 +0100

    fixed betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * @ac: ac
+ * @av: argument vector
+ * Return: return 0 on success
+ */
 int main(int ac, char **av)
 {
 	printf("%s\n", av[ac * 0]);
