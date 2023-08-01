af8864278819ca854d208ad9c028b34c3934df0d
Kariaki58 <kariakichernocherno@gmail.com>
2023-08-01 15:20:15 +0100

    function that retuns a pointer to newly allocated space in meory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicated a string
+ * @str: string
+ * Return: return dup of string
+ */
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
