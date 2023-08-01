9a428599d66515f7be31b6e0f6b2f49167ebc72c
Kariaki58 <kariakichernocherno@gmail.com>
2023-08-01 21:37:40 +0100

    prints array of integers dynamically

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,25 +4,33 @@
 /**
  * alloc_grid - allocates a grid
  * @width: width
- * @height - height
+ * @height: height
  * Return: grid
  */
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-	int i, j;
+	int i, j, pos;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = (int **)malloc(height * sizeof(int *));
-	if (!grid)
+	if (grid == NULL)
 	{
 		free(grid);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
+	{
 		grid[i] = (int *)malloc(width * sizeof(int));
-
+		if (grid[i] == NULL)
+		{
+			for (pos = i; pos >= 0; pos--)
+				free(grid[pos]);
+			free(grid);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
