83251ebfd005944bae0bcde83b5889ebb4881068
Kariaki58 <kariakichernocherno@gmail.com>
2023-09-04 13:16:04 +0100

    I will show you how great I am

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * argstostr - its the pebble in your shoe
+ * @ac: ac
+ * @av: av
+ * Return: a pointer to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *string;
+	int i, j;
+
+	if (!ac || !av)
+		return (NULL);
+	string = malloc(sizeof(*av) * ac);
+	if (string == NULL)
+		return (NULL);
+	for (i = j = 0; i < ac; i++)
+	{
+		while (*av[i] != '\0')
+		{
+			string[j++] = *av[i];
+			av[i]++;
+		}
+		string[j++] = '\n';
+	}
+	string[j] = '\0';
+	return (string);
+}
