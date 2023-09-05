7894d21770f96270be755212cd5993673fd929d8
Kariaki58 <kariakichernocherno@gmail.com>
2023-09-05 04:00:30 +0100

    callocation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - using calloc
+ * @nmemb: nmemb
+ * @size: size
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	return ptr;
+}
