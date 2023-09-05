8774524d90457926cc25d71226643e8f470792a8
Kariaki58 <Kariakichernocherno@gmail.com>
2023-09-05 03:20:36 +0100

    unsolved problem

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: s1
+ * @s2: s2
+ * @n: n
+ * Return: string concate
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	
+}
