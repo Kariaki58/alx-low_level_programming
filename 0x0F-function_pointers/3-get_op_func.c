ba9516395a8aeea20b1213377e56122eb2a7796d
Kariaki58 <kariakichernocherno@gmail.com>
2023-09-07 08:23:53 +0100

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+#include <string.h>
+
+int (*get_op_func(char *s)) (int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (!strcmp(s, ops[i].op))
+			return (ops[i].f);
+		i++;
+	}
+	return (0);
+}
