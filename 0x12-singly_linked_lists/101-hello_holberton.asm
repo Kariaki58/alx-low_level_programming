cc0814a3f6226c0219024621fd30c61c7ae87616
Kariaki58 <kariakichernocherno@gmail.com>
2023-09-07 18:38:12 +0100

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,21 @@
-          global    main
-          extern    printf
+section .data
+    hello_message db "Hello, Holberton",0
+    format db "%s",10,0
+
+section .text
+    global main
+    extern printf
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+    sub rsp, 8            ; Align the stack (16-byte alignment)
+    mov rdi, format       ; Load the format string address
+    mov rsi, hello_message ; Load the message address
+    call printf           ; Call the printf function
+
+    add rsp, 8            ; Restore the stack
+
+    ; Exit the program
+    mov rax, 60           ; syscall number for exit
+    xor rdi, rdi          ; exit status: 0
+    syscall
+
