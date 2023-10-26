55b948a410a82ac5abc25ba31841d846243797e8
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-26 15:58:56 +0100

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,28 +4,24 @@
 
 def discover_island(grid):
     """Discover island"""
-    if any(grid[0]) or any(grid[-1]):
-        return False
-    for row in grid:
-        if row[0] == 1 or row[-1] == 1:
-            return False
-    return True
+    adder = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        adder += 1
+    if j <= 0 or not grid[i][j - 1]:
+        adder += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        adder += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        adder += 1
+    return adder
 
 
 def island_perimeter(grid):
     """Find perimeter of the island in a river"""
-    len_rows = len(grid)
-    len_cols = len(grid[0])
     perimeter = 0
-    if not discover_island(grid):
-        return perimeter
-
-    for i in range(len_rows):
-        for j in range(len_cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += discover_island(grid, i, j)
     return perimeter
