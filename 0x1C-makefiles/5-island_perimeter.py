b992b1c954507f1762b7e46eecab91d6252739e1
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-26 15:55:50 +0100

    new solution

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,45 +1,31 @@
 #!/usr/bin/python3
-"""function that finds a island"""
+"""modules"""
 
 
-def descover_island(grid):
-    """descover island"""
-    if any(grid[0][:]):
+def discover_island(grid):
+    """Discover island"""
+    if any(grid[0]) or any(grid[-1]):
         return False
-    if any(grid[:][0]):
-        return False
-    if any(grid[len(grid) - 1][:]):
-        return False
-    if any(grid[:][len(grid) - 1]):
-        return False
+    for row in grid:
+        if row[0] == 1 or row[-1] == 1:
+            return False
     return True
 
 
 def island_perimeter(grid):
-    """find island in a river"""
+    """Find perimeter of the island in a river"""
     len_rows = len(grid)
     len_cols = len(grid[0])
-    rows = 0
-    cols = 0
-    adder = 0
-    grid_check = True
-    if not descover_island(grid):
-        return adder
+    perimeter = 0
+    if not discover_island(grid):
+        return perimeter
+
     for i in range(len_rows):
         for j in range(len_cols):
-            if grid[i][j] == 1 and grid_check:
-                rows = i
-                cols = j
-                while grid[rows][cols] == 1:
-                    if rows <= len_rows and cols <= len_cols:
-                        grid_check = False
-                        adder += grid[rows][cols]
-                        rows += 1
-                cols = j
-                rows = rows - 1
-                while grid[rows][cols] == 1:
-                    if rows <= len_rows and cols <= len_cols:
-                        grid_check = False
-                        adder += grid[rows][cols]
-                        cols += 1
-    return 2 * adder
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
