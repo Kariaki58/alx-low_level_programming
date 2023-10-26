c9b29d05806ddb9ced57e0fdada8a7a36d4ce484
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-26 14:52:21 +0100

    finding island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """find island in a river"""
+    len_rows = len(grid)
+    len_cols = len(grid[0])
+    rows = 0
+    cols = 0
+    adder = 0
+    grid_check = True
+    for i in range(len_rows):
+        for j in range(len_cols):
+            if grid[i][j] == 1 and grid_check:
+                rows = i
+                cols = j
+                while grid[rows][cols] == 1:
+                    if rows <= len_rows and cols <= len_cols:
+                        grid_check = False
+                        adder += grid[rows][cols]
+                        rows += 1
+                cols = j
+                rows = rows - 1
+                while grid[rows][cols] == 1:
+                    if rows <= len_rows and cols <= len_cols:
+                        grid_check = False
+                        adder += grid[rows][cols]
+                        cols += 1
+    return 2 * adder
