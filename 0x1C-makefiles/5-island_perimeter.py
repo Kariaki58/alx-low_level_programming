6ca094fbe36da6733403bebfc2d02743bccbf2d7
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-26 16:00:25 +0100

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,41 @@
-#!/usr/bin/python3
-"""modules"""
-
-
-def discover_island(grid):
-    """Discover island"""
-    adder = 0
-
-    if i <= 0 or not grid[i - 1][j]:
-        adder += 1
-    if j <= 0 or not grid[i][j - 1]:
-        adder += 1
-    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-        adder += 1
-    if i >= len(grid) - 1 or not grid[i + 1][j]:
-        adder += 1
-    return adder
+def descover_island(grid):
+    """descover island"""
+    if any(grid[0][:]):
+        return False
+    if any(grid[:][0]):
+        return False
+    if any(grid[len(grid) - 1][:]):
+        return False
+    if any(grid[:][len(grid) - 1]):
+        return False
+    return True
 
 
 def island_perimeter(grid):
-    """Find perimeter of the island in a river"""
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j]:
-                perimeter += discover_island(grid, i, j)
-    return perimeter
+    """find island in a river"""
+    len_rows = len(grid)
+    len_cols = len(grid[0])
+    rows = 0
+    cols = 0
+    adder = 0
+    grid_check = True
+    if not descover_island(grid):
+        return adder
+    for i in range(len_rows):
+        for j in range(len_cols):
+            if grid[i][j] == 1 and grid_check:
+                rows = i
+                cols = j
+                while grid[rows][cols] == 1:
+                    if rows <= len_rows and cols <= len_cols:
+                        grid_check = False
+                        adder += grid[rows][cols]
+                        rows += 1
+                cols = j
+                rows = rows - 1
+                while grid[rows][cols] == 1:
+                    if rows <= len_rows and cols <= len_cols:
+                        grid_check = False
+                        adder += grid[rows][cols]
+                        cols += 1
+    return 2 * adder
