7f2a8c157310674252f43e237f140acaf23634c1
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-16 08:40:30 -0800

    fixed betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,15 +9,15 @@
  */
 int *array_range(int min, int max)
 {
-        int *array, i, j;
+	int *array, i, j;
 
-        if (min > max)
-                return (NULL);
-        array = malloc(sizeof(int) * ((max + 1) - min));
-        if (!array)
-                return (NULL);
-        j = 0;
-        for (i = 0; i < ((max + 1) - min); i++)
-                array[j++] = min + i;
-        return (array);
+	if (min > max)
+		return (NULL);
+	array = malloc(sizeof(int) * ((max + 1) - min));
+	if (!array)
+		return (NULL);
+	j = 0;
+	for (i = 0; i < ((max + 1) - min); i++)
+		array[j++] = min + i;
+	return (array);
 }
